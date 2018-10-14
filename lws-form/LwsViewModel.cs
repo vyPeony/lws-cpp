321dd09094293158301a552b4672fdbddf1cb16f
vyPeony <vayyikra@gmail.com>
2018-10-14 23:57:49 +0900

    Clean up code

--- lws-form/LwsViewModel.cs
+++ lws-form/LwsViewModel.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reactive.Disposables;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using Reactive.Bindings;
+using System;
+using System.Reactive.Disposables;
 
 namespace lws_form
 {
     public class LwsViewModel : BindableBase, IDisposable
     {
+        private LwsModel model = null;
         private CompositeDisposable Disposable { get; } = new CompositeDisposable();
         public ReactiveCommand ExecuteCommand { get; } = new ReactiveCommand();
         public ReadOnlyReactiveCollection<EnchantData> Output { get; }
@@ -22,10 +17,10 @@ namespace lws_form
             model = new LwsModel();
             Output = model.Output.ToReadOnlyReactiveCollection();
             ExecuteCommand.Subscribe(_ => Execute());
+
+            Disposable.Add(Output);
             Disposable.Add(ExecuteCommand);
         }
-
-        private LwsModel model = null;
 
         private void Execute()
         {
